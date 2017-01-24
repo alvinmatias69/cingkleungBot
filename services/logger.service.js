8503f335bd278b89500bfb4fd8ff0b1d925178f8
Matias <alvinmatias69@gmail.com>
2017-01-24 22:58:41 +0700

    logged data changed

--- services/logger.service.js
+++ services/logger.service.js
@@ -6,16 +6,17 @@ const fs = require('fs');
 
 var loggerService = function(req, res, next) {
 	if (!fs.existsSync('log/log.txt')) {
-		let header = 'date,query,user\n';
+		let header = 'date,query,user,username\n';
 		fs.writeFileSync('log/log.txt', header, {flag : 'w'});
 	}
 
 	const message = req.body;
 	let logData = new Date() + ',';
+	console.log(message);
 	if (message.hasOwnProperty('callback_query')) {
-		logData = logData + message.callback_query.data + ',' + message.callback_query.from.id + '\n';
+		logData = logData + message.callback_query.data + ',' + message.callback_query.from.id + ',' + message.callback_query.from.username + '\n';
 	}else{
-		logData = logData + message.message.text + ',' + message.message.from.id + '\n';
+		logData = logData + message.message.text + ',' + message.message.from.id + ',' + message.callback_query.from.username + '\n';
 	}
 	fs.appendFile('log/log.txt', logData);
 	next();
